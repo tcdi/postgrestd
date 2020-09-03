the8472/rust

use memmove instead of generic in-place iteration for IntoIter source
this is the original SpecExtend<_, IntoIter> logic except generalizing
the fast-path to include a memmove

src/liballoc/vec.rs
@@ -2078,9 +2078,15 @@ impl<T> SpecFrom<T, IntoIter<T>> for Vec<T> {
         // A common case is passing a vector into a function which immediately
         // re-collects into a vector. We can short circuit this if the IntoIter
         // has not been advanced at all.
-        if iterator.buf.as_ptr() as *const _ == iterator.ptr {
+        // We can also reuse the memory and move the data to the front if
+        // allocating a new vector and moving to it would result in the same capacity
+        let non_zero_offset = iterator.buf.as_ptr() as *const _ != iterator.ptr;
+        if !non_zero_offset || iterator.len() >= iterator.cap / 2 {
             unsafe {
                 let iterator = mem::ManuallyDrop::new(iterator);
+                if non_zero_offset {
+                    ptr::copy(iterator.ptr, iterator.buf.as_ptr(), iterator.len());
+                }
                 let vec = Vec::from_raw_parts(iterator.buf.as_ptr(),
                                               iterator.len(),
                                               iterator.cap);
@@ -2088,7 +2094,9 @@ impl<T> SpecFrom<T, IntoIter<T>> for Vec<T> {
             }
         }
 
-        from_into_iter_source(iterator)
+        let mut vec = Vec::new();
+        vec.extend(iterator);
+        vec
     }
 }
 

tcdi/postgrestd

alloc/src/vec.rs
@@ -2216,14 +2216,22 @@ impl<T> SpecFrom<T, IntoIter<T>> for Vec<T> {
         // A common case is passing a vector into a function which immediately
         // re-collects into a vector. We can short circuit this if the IntoIter
         // has not been advanced at all.
-        if iterator.buf.as_ptr() as *const _ == iterator.ptr {
+        // We can also reuse the memory and move the data to the front if
+        // allocating a new vector and moving to it would result in the same capacity
+        let non_zero_offset = iterator.buf.as_ptr() as *const _ != iterator.ptr;
+        if !non_zero_offset || iterator.len() >= iterator.cap / 2 {
             unsafe {
                 let it = ManuallyDrop::new(iterator);
+                if non_zero_offset {
+                    ptr::copy(it.ptr, it.buf.as_ptr(), it.len());
+                }
                 return Vec::from_raw_parts(it.buf.as_ptr(), it.len(), it.cap);
             }
         }
 
-        from_into_iter_source(iterator)
+        let mut vec = Vec::new();
+        vec.extend(iterator);
+        vec
     }
 }
 