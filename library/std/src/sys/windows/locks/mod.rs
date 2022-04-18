05df247016405299a6204b97e79f2b8d02bf07c0
bors <bors@rust-lang.org>
2022-04-18 12:15:39 +0000

    Auto merge of #96042 - m-ou-se:one-reentrant-mutex, r=Amanieu
    Use a single ReentrantMutex implementation on all platforms.
    
    This replaces all platform specific ReentrantMutex implementations by the one I added in #95727 for Linux, since that one does not depend on any platform specific details.
    
    r? `@Amanieu`

--- library/std/src/sys/windows/locks/mod.rs
+++ library/std/src/sys/windows/locks/mod.rs
@@ -2,5 +2,10 @@ mod condvar;
 mod mutex;
 mod rwlock;
 pub use condvar::{Condvar, MovableCondvar};
+<<<<<<< HEAD:library/std/src/sys/windows/locks/mod.rs
 pub use mutex::{MovableMutex, Mutex, ReentrantMutex};
 pub use rwlock::{MovableRWLock, RWLock};
+=======
+pub use mutex::{MovableMutex, Mutex};
+pub use rwlock::{MovableRwLock, RwLock};
+>>>>>>> 9e0f0c856 (Auto merge of #96042 - m-ou-se:one-reentrant-mutex, r=Amanieu):std/src/sys/windows/locks/mod.rs
