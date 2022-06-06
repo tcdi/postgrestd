2c37fcf7134ba563631fc69fc24cccb600d4c2b8
Jubilee Young <j_young@tcdi.com>
2022-06-06 08:14:46 -0700

    DROP TABLE std

--- std/src/os.rs
+++
@@ -1,9 +0,0 @@
-#![stable(feature = "rust1", since = "1.0.0")]
-
-#[stable(feature = "rust1", since = "1.0.0")]
-pub mod raw {
-    #[stable(feature = "rust1", since = "1.0.0")]
-    pub use core::ffi::{c_void, c_char, c_schar, c_uchar,
-        c_double, c_float, c_int, c_long, c_longlong, c_short,
-        c_uint, c_ulong, c_ulonglong, c_ushort};
-}
--- std/src/sys.rs
+++
@@ -1,1 +0,0 @@
-#![stable(feature = "rust1", since = "1.0.0")]
