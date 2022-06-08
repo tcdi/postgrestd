a2bd8b8ad353d0329d6b901fe8c126f6506daa06
Jubilee Young <j_young@tcdi.com>
2022-06-08 14:43:28 -0700

    alloc

--- library/std/src/alloc.rs
+++ library/std/src/alloc.rs
@@ -127,161 +127,161 @@ pub use alloc_crate::alloc::*;
 #[derive(Debug, Default, Copy, Clone)]
 pub struct System;
 
-impl System {
-    #[inline]
-    fn alloc_impl(&self, layout: Layout, zeroed: bool) -> Result<NonNull<[u8]>, AllocError> {
-        match layout.size() {
-            0 => Ok(NonNull::slice_from_raw_parts(layout.dangling(), 0)),
-            // SAFETY: `layout` is non-zero in size,
-            size => unsafe {
-                let raw_ptr = if zeroed {
-                    GlobalAlloc::alloc_zeroed(self, layout)
-                } else {
-                    GlobalAlloc::alloc(self, layout)
-                };
-                let ptr = NonNull::new(raw_ptr).ok_or(AllocError)?;
-                Ok(NonNull::slice_from_raw_parts(ptr, size))
-            },
-        }
-    }
+// impl System {
+//     #[inline]
+//     fn alloc_impl(&self, layout: Layout, zeroed: bool) -> Result<NonNull<[u8]>, AllocError> {
+//         match layout.size() {
+//             0 => Ok(NonNull::slice_from_raw_parts(layout.dangling(), 0)),
+//             // SAFETY: `layout` is non-zero in size,
+//             size => unsafe {
+//                 let raw_ptr = if zeroed {
+//                     GlobalAlloc::alloc_zeroed(self, layout)
+//                 } else {
+//                     GlobalAlloc::alloc(self, layout)
+//                 };
+//                 let ptr = NonNull::new(raw_ptr).ok_or(AllocError)?;
+//                 Ok(NonNull::slice_from_raw_parts(ptr, size))
+//             },
+//         }
+//     }
 
-    // SAFETY: Same as `Allocator::grow`
-    #[inline]
-    unsafe fn grow_impl(
-        &self,
-        ptr: NonNull<u8>,
-        old_layout: Layout,
-        new_layout: Layout,
-        zeroed: bool,
-    ) -> Result<NonNull<[u8]>, AllocError> {
-        debug_assert!(
-            new_layout.size() >= old_layout.size(),
-            "`new_layout.size()` must be greater than or equal to `old_layout.size()`"
-        );
+//     // SAFETY: Same as `Allocator::grow`
+//     #[inline]
+//     unsafe fn grow_impl(
+//         &self,
+//         ptr: NonNull<u8>,
+//         old_layout: Layout,
+//         new_layout: Layout,
+//         zeroed: bool,
+//     ) -> Result<NonNull<[u8]>, AllocError> {
+//         debug_assert!(
+//             new_layout.size() >= old_layout.size(),
+//             "`new_layout.size()` must be greater than or equal to `old_layout.size()`"
+//         );
 
-        match old_layout.size() {
-            0 => self.alloc_impl(new_layout, zeroed),
+//         match old_layout.size() {
+//             0 => self.alloc_impl(new_layout, zeroed),
 
-            // SAFETY: `new_size` is non-zero as `new_size` is greater than or equal to `old_size`
-            // as required by safety conditions and the `old_size == 0` case was handled in the
-            // previous match arm. Other conditions must be upheld by the caller
-            old_size if old_layout.align() == new_layout.align() => unsafe {
-                let new_size = new_layout.size();
+//             // SAFETY: `new_size` is non-zero as `new_size` is greater than or equal to `old_size`
+//             // as required by safety conditions and the `old_size == 0` case was handled in the
+//             // previous match arm. Other conditions must be upheld by the caller
+//             old_size if old_layout.align() == new_layout.align() => unsafe {
+//                 let new_size = new_layout.size();
 
-                // `realloc` probably checks for `new_size >= old_layout.size()` or something similar.
-                intrinsics::assume(new_size >= old_layout.size());
+//                 // `realloc` probably checks for `new_size >= old_layout.size()` or something similar.
+//                 intrinsics::assume(new_size >= old_layout.size());
 
-                let raw_ptr = GlobalAlloc::realloc(self, ptr.as_ptr(), old_layout, new_size);
-                let ptr = NonNull::new(raw_ptr).ok_or(AllocError)?;
-                if zeroed {
-                    raw_ptr.add(old_size).write_bytes(0, new_size - old_size);
-                }
-                Ok(NonNull::slice_from_raw_parts(ptr, new_size))
-            },
+//                 let raw_ptr = GlobalAlloc::realloc(self, ptr.as_ptr(), old_layout, new_size);
+//                 let ptr = NonNull::new(raw_ptr).ok_or(AllocError)?;
+//                 if zeroed {
+//                     raw_ptr.add(old_size).write_bytes(0, new_size - old_size);
+//                 }
+//                 Ok(NonNull::slice_from_raw_parts(ptr, new_size))
+//             },
 
-            // SAFETY: because `new_layout.size()` must be greater than or equal to `old_size`,
-            // both the old and new memory allocation are valid for reads and writes for `old_size`
-            // bytes. Also, because the old allocation wasn't yet deallocated, it cannot overlap
-            // `new_ptr`. Thus, the call to `copy_nonoverlapping` is safe. The safety contract
-            // for `dealloc` must be upheld by the caller.
-            old_size => unsafe {
-                let new_ptr = self.alloc_impl(new_layout, zeroed)?;
-                ptr::copy_nonoverlapping(ptr.as_ptr(), new_ptr.as_mut_ptr(), old_size);
-                Allocator::deallocate(&self, ptr, old_layout);
-                Ok(new_ptr)
-            },
-        }
-    }
-}
+//             // SAFETY: because `new_layout.size()` must be greater than or equal to `old_size`,
+//             // both the old and new memory allocation are valid for reads and writes for `old_size`
+//             // bytes. Also, because the old allocation wasn't yet deallocated, it cannot overlap
+//             // `new_ptr`. Thus, the call to `copy_nonoverlapping` is safe. The safety contract
+//             // for `dealloc` must be upheld by the caller.
+//             old_size => unsafe {
+//                 let new_ptr = self.alloc_impl(new_layout, zeroed)?;
+//                 ptr::copy_nonoverlapping(ptr.as_ptr(), new_ptr.as_mut_ptr(), old_size);
+//                 Allocator::deallocate(&self, ptr, old_layout);
+//                 Ok(new_ptr)
+//             },
+//         }
+//     }
+// }
 
-// The Allocator impl checks the layout size to be non-zero and forwards to the GlobalAlloc impl,
-// which is in `std::sys::*::alloc`.
-#[unstable(feature = "allocator_api", issue = "32838")]
-unsafe impl Allocator for System {
-    #[inline]
-    fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        self.alloc_impl(layout, false)
-    }
+// // The Allocator impl checks the layout size to be non-zero and forwards to the GlobalAlloc impl,
+// // which is in `std::sys::*::alloc`.
+// #[unstable(feature = "allocator_api", issue = "32838")]
+// unsafe impl Allocator for System {
+//     #[inline]
+//     fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+//         self.alloc_impl(layout, false)
+//     }
 
-    #[inline]
-    fn allocate_zeroed(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        self.alloc_impl(layout, true)
-    }
+//     #[inline]
+//     fn allocate_zeroed(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+//         self.alloc_impl(layout, true)
+//     }
 
-    #[inline]
-    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
-        if layout.size() != 0 {
-            // SAFETY: `layout` is non-zero in size,
-            // other conditions must be upheld by the caller
-            unsafe { GlobalAlloc::dealloc(self, ptr.as_ptr(), layout) }
-        }
-    }
+//     #[inline]
+//     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
+//         if layout.size() != 0 {
+//             // SAFETY: `layout` is non-zero in size,
+//             // other conditions must be upheld by the caller
+//             unsafe { GlobalAlloc::dealloc(self, ptr.as_ptr(), layout) }
+//         }
+//     }
 
-    #[inline]
-    unsafe fn grow(
-        &self,
-        ptr: NonNull<u8>,
-        old_layout: Layout,
-        new_layout: Layout,
-    ) -> Result<NonNull<[u8]>, AllocError> {
-        // SAFETY: all conditions must be upheld by the caller
-        unsafe { self.grow_impl(ptr, old_layout, new_layout, false) }
-    }
+//     #[inline]
+//     unsafe fn grow(
+//         &self,
+//         ptr: NonNull<u8>,
+//         old_layout: Layout,
+//         new_layout: Layout,
+//     ) -> Result<NonNull<[u8]>, AllocError> {
+//         // SAFETY: all conditions must be upheld by the caller
+//         unsafe { self.grow_impl(ptr, old_layout, new_layout, false) }
+//     }
 
-    #[inline]
-    unsafe fn grow_zeroed(
-        &self,
-        ptr: NonNull<u8>,
-        old_layout: Layout,
-        new_layout: Layout,
-    ) -> Result<NonNull<[u8]>, AllocError> {
-        // SAFETY: all conditions must be upheld by the caller
-        unsafe { self.grow_impl(ptr, old_layout, new_layout, true) }
-    }
+//     #[inline]
+//     unsafe fn grow_zeroed(
+//         &self,
+//         ptr: NonNull<u8>,
+//         old_layout: Layout,
+//         new_layout: Layout,
+//     ) -> Result<NonNull<[u8]>, AllocError> {
+//         // SAFETY: all conditions must be upheld by the caller
+//         unsafe { self.grow_impl(ptr, old_layout, new_layout, true) }
+//     }
 
-    #[inline]
-    unsafe fn shrink(
-        &self,
-        ptr: NonNull<u8>,
-        old_layout: Layout,
-        new_layout: Layout,
-    ) -> Result<NonNull<[u8]>, AllocError> {
-        debug_assert!(
-            new_layout.size() <= old_layout.size(),
-            "`new_layout.size()` must be smaller than or equal to `old_layout.size()`"
-        );
+//     #[inline]
+//     unsafe fn shrink(
+//         &self,
+//         ptr: NonNull<u8>,
+//         old_layout: Layout,
+//         new_layout: Layout,
+//     ) -> Result<NonNull<[u8]>, AllocError> {
+//         debug_assert!(
+//             new_layout.size() <= old_layout.size(),
+//             "`new_layout.size()` must be smaller than or equal to `old_layout.size()`"
+//         );
 
-        match new_layout.size() {
-            // SAFETY: conditions must be upheld by the caller
-            0 => unsafe {
-                Allocator::deallocate(&self, ptr, old_layout);
-                Ok(NonNull::slice_from_raw_parts(new_layout.dangling(), 0))
-            },
+//         match new_layout.size() {
+//             // SAFETY: conditions must be upheld by the caller
+//             0 => unsafe {
+//                 Allocator::deallocate(&self, ptr, old_layout);
+//                 Ok(NonNull::slice_from_raw_parts(new_layout.dangling(), 0))
+//             },
 
-            // SAFETY: `new_size` is non-zero. Other conditions must be upheld by the caller
-            new_size if old_layout.align() == new_layout.align() => unsafe {
-                // `realloc` probably checks for `new_size <= old_layout.size()` or something similar.
-                intrinsics::assume(new_size <= old_layout.size());
+//             // SAFETY: `new_size` is non-zero. Other conditions must be upheld by the caller
+//             new_size if old_layout.align() == new_layout.align() => unsafe {
+//                 // `realloc` probably checks for `new_size <= old_layout.size()` or something similar.
+//                 intrinsics::assume(new_size <= old_layout.size());
 
-                let raw_ptr = GlobalAlloc::realloc(self, ptr.as_ptr(), old_layout, new_size);
-                let ptr = NonNull::new(raw_ptr).ok_or(AllocError)?;
-                Ok(NonNull::slice_from_raw_parts(ptr, new_size))
-            },
+//                 let raw_ptr = GlobalAlloc::realloc(self, ptr.as_ptr(), old_layout, new_size);
+//                 let ptr = NonNull::new(raw_ptr).ok_or(AllocError)?;
+//                 Ok(NonNull::slice_from_raw_parts(ptr, new_size))
+//             },
 
-            // SAFETY: because `new_size` must be smaller than or equal to `old_layout.size()`,
-            // both the old and new memory allocation are valid for reads and writes for `new_size`
-            // bytes. Also, because the old allocation wasn't yet deallocated, it cannot overlap
-            // `new_ptr`. Thus, the call to `copy_nonoverlapping` is safe. The safety contract
-            // for `dealloc` must be upheld by the caller.
-            new_size => unsafe {
-                let new_ptr = Allocator::allocate(&self, new_layout)?;
-                ptr::copy_nonoverlapping(ptr.as_ptr(), new_ptr.as_mut_ptr(), new_size);
-                Allocator::deallocate(&self, ptr, old_layout);
-                Ok(new_ptr)
-            },
-        }
-    }
-}
+//             // SAFETY: because `new_size` must be smaller than or equal to `old_layout.size()`,
+//             // both the old and new memory allocation are valid for reads and writes for `new_size`
+//             // bytes. Also, because the old allocation wasn't yet deallocated, it cannot overlap
+//             // `new_ptr`. Thus, the call to `copy_nonoverlapping` is safe. The safety contract
+//             // for `dealloc` must be upheld by the caller.
+//             new_size => unsafe {
+//                 let new_ptr = Allocator::allocate(&self, new_layout)?;
+//                 ptr::copy_nonoverlapping(ptr.as_ptr(), new_ptr.as_mut_ptr(), new_size);
+//                 Allocator::deallocate(&self, ptr, old_layout);
+//                 Ok(new_ptr)
+//             },
+//         }
+//     }
+// }
 
 static HOOK: AtomicPtr<()> = AtomicPtr::new(ptr::null_mut());
 
@@ -323,7 +323,7 @@ fn default_alloc_error_hook(layout: Layout) {
     if unsafe { __rust_alloc_error_handler_should_panic != 0 } {
         panic!("memory allocation of {} bytes failed\n", layout.size());
     } else {
-        rtprintpanic!("memory allocation of {} bytes failed\n", layout.size());
+        // rtprintpanic!("memory allocation of {} bytes failed\n", layout.size());
     }
 }
 
@@ -336,64 +336,65 @@ pub fn rust_oom(layout: Layout) -> ! {
     let hook: fn(Layout) =
         if hook.is_null() { default_alloc_error_hook } else { unsafe { mem::transmute(hook) } };
     hook(layout);
-    crate::process::abort()
+    // crate::process::abort()
+    core::intrinsics::abort()
 }
 
-#[cfg(not(test))]
-#[doc(hidden)]
-#[allow(unused_attributes)]
-#[unstable(feature = "alloc_internals", issue = "none")]
-pub mod __default_lib_allocator {
-    use super::{GlobalAlloc, Layout, System};
-    // These magic symbol names are used as a fallback for implementing the
-    // `__rust_alloc` etc symbols (see `src/liballoc/alloc.rs`) when there is
-    // no `#[global_allocator]` attribute.
+// #[cfg(not(test))]
+// #[doc(hidden)]
+// #[allow(unused_attributes)]
+// #[unstable(feature = "alloc_internals", issue = "none")]
+// pub mod __default_lib_allocator {
+//     use super::{GlobalAlloc, Layout, System};
+//     // These magic symbol names are used as a fallback for implementing the
+//     // `__rust_alloc` etc symbols (see `src/liballoc/alloc.rs`) when there is
+//     // no `#[global_allocator]` attribute.
 
-    // for symbol names src/librustc_ast/expand/allocator.rs
-    // for signatures src/librustc_allocator/lib.rs
+//     // for symbol names src/librustc_ast/expand/allocator.rs
+//     // for signatures src/librustc_allocator/lib.rs
 
-    // linkage directives are provided as part of the current compiler allocator
-    // ABI
+//     // linkage directives are provided as part of the current compiler allocator
+//     // ABI
 
-    #[rustc_std_internal_symbol]
-    pub unsafe extern "C" fn __rdl_alloc(size: usize, align: usize) -> *mut u8 {
-        // SAFETY: see the guarantees expected by `Layout::from_size_align` and
-        // `GlobalAlloc::alloc`.
-        unsafe {
-            let layout = Layout::from_size_align_unchecked(size, align);
-            System.alloc(layout)
-        }
-    }
+//     #[rustc_std_internal_symbol]
+//     pub unsafe extern "C" fn __rdl_alloc(size: usize, align: usize) -> *mut u8 {
+//         // SAFETY: see the guarantees expected by `Layout::from_size_align` and
+//         // `GlobalAlloc::alloc`.
+//         unsafe {
+//             let layout = Layout::from_size_align_unchecked(size, align);
+//             System.alloc(layout)
+//         }
+//     }
 
-    #[rustc_std_internal_symbol]
-    pub unsafe extern "C" fn __rdl_dealloc(ptr: *mut u8, size: usize, align: usize) {
-        // SAFETY: see the guarantees expected by `Layout::from_size_align` and
-        // `GlobalAlloc::dealloc`.
-        unsafe { System.dealloc(ptr, Layout::from_size_align_unchecked(size, align)) }
-    }
+//     #[rustc_std_internal_symbol]
+//     pub unsafe extern "C" fn __rdl_dealloc(ptr: *mut u8, size: usize, align: usize) {
+//         // SAFETY: see the guarantees expected by `Layout::from_size_align` and
+//         // `GlobalAlloc::dealloc`.
+//         unsafe { System.dealloc(ptr, Layout::from_size_align_unchecked(size, align)) }
+//     }
 
-    #[rustc_std_internal_symbol]
-    pub unsafe extern "C" fn __rdl_realloc(
-        ptr: *mut u8,
-        old_size: usize,
-        align: usize,
-        new_size: usize,
-    ) -> *mut u8 {
-        // SAFETY: see the guarantees expected by `Layout::from_size_align` and
-        // `GlobalAlloc::realloc`.
-        unsafe {
-            let old_layout = Layout::from_size_align_unchecked(old_size, align);
-            System.realloc(ptr, old_layout, new_size)
-        }
-    }
+//     #[rustc_std_internal_symbol]
+//     pub unsafe extern "C" fn __rdl_realloc(
+//         ptr: *mut u8,
+//         old_size: usize,
+//         align: usize,
+//         new_size: usize,
+//     ) -> *mut u8 {
+//         // SAFETY: see the guarantees expected by `Layout::from_size_align` and
+//         // `GlobalAlloc::realloc`.
+//         unsafe {
+//             let old_layout = Layout::from_size_align_unchecked(old_size, align);
+//             System.realloc(ptr, old_layout, new_size)
+//         }
+//     }
 
-    #[rustc_std_internal_symbol]
-    pub unsafe extern "C" fn __rdl_alloc_zeroed(size: usize, align: usize) -> *mut u8 {
-        // SAFETY: see the guarantees expected by `Layout::from_size_align` and
-        // `GlobalAlloc::alloc_zeroed`.
-        unsafe {
-            let layout = Layout::from_size_align_unchecked(size, align);
-            System.alloc_zeroed(layout)
-        }
-    }
-}
+//     #[rustc_std_internal_symbol]
+//     pub unsafe extern "C" fn __rdl_alloc_zeroed(size: usize, align: usize) -> *mut u8 {
+//         // SAFETY: see the guarantees expected by `Layout::from_size_align` and
+//         // `GlobalAlloc::alloc_zeroed`.
+//         unsafe {
+//             let layout = Layout::from_size_align_unchecked(size, align);
+//             System.alloc_zeroed(layout)
+//         }
+//     }
+// }
