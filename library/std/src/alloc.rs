cb6bd8fc13f4ba64bbc5982bc044d8840b7c0654
Jubilee Young <j_young@tcdi.com>
2022-06-08 19:42:25 -0700

    Reenable system alloc and pass cargo build

--- library/std/src/alloc.rs
+++ library/std/src/alloc.rs
@@ -340,61 +340,61 @@ pub fn rust_oom(layout: Layout) -> ! {
     core::intrinsics::abort()
 }
 
-// #[cfg(not(test))]
-// #[doc(hidden)]
-// #[allow(unused_attributes)]
-// #[unstable(feature = "alloc_internals", issue = "none")]
-// pub mod __default_lib_allocator {
-//     use super::{GlobalAlloc, Layout, System};
-//     // These magic symbol names are used as a fallback for implementing the
-//     // `__rust_alloc` etc symbols (see `src/liballoc/alloc.rs`) when there is
-//     // no `#[global_allocator]` attribute.
+#[cfg(not(test))]
+#[doc(hidden)]
+#[allow(unused_attributes)]
+#[unstable(feature = "alloc_internals", issue = "none")]
+pub mod __default_lib_allocator {
+    use super::{GlobalAlloc, Layout, System};
+    // These magic symbol names are used as a fallback for implementing the
+    // `__rust_alloc` etc symbols (see `src/liballoc/alloc.rs`) when there is
+    // no `#[global_allocator]` attribute.
 
-//     // for symbol names src/librustc_ast/expand/allocator.rs
-//     // for signatures src/librustc_allocator/lib.rs
+    // for symbol names src/librustc_ast/expand/allocator.rs
+    // for signatures src/librustc_allocator/lib.rs
 
-//     // linkage directives are provided as part of the current compiler allocator
-//     // ABI
+    // linkage directives are provided as part of the current compiler allocator
+    // ABI
 
-//     #[rustc_std_internal_symbol]
-//     pub unsafe extern "C" fn __rdl_alloc(size: usize, align: usize) -> *mut u8 {
-//         // SAFETY: see the guarantees expected by `Layout::from_size_align` and
-//         // `GlobalAlloc::alloc`.
-//         unsafe {
-//             let layout = Layout::from_size_align_unchecked(size, align);
-//             System.alloc(layout)
-//         }
-//     }
+    #[rustc_std_internal_symbol]
+    pub unsafe extern "C" fn __rdl_alloc(size: usize, align: usize) -> *mut u8 {
+        // SAFETY: see the guarantees expected by `Layout::from_size_align` and
+        // `GlobalAlloc::alloc`.
+        unsafe {
+            let layout = Layout::from_size_align_unchecked(size, align);
+            System.alloc(layout)
+        }
+    }
 
-//     #[rustc_std_internal_symbol]
-//     pub unsafe extern "C" fn __rdl_dealloc(ptr: *mut u8, size: usize, align: usize) {
-//         // SAFETY: see the guarantees expected by `Layout::from_size_align` and
-//         // `GlobalAlloc::dealloc`.
-//         unsafe { System.dealloc(ptr, Layout::from_size_align_unchecked(size, align)) }
-//     }
+    #[rustc_std_internal_symbol]
+    pub unsafe extern "C" fn __rdl_dealloc(ptr: *mut u8, size: usize, align: usize) {
+        // SAFETY: see the guarantees expected by `Layout::from_size_align` and
+        // `GlobalAlloc::dealloc`.
+        unsafe { System.dealloc(ptr, Layout::from_size_align_unchecked(size, align)) }
+    }
 
-//     #[rustc_std_internal_symbol]
-//     pub unsafe extern "C" fn __rdl_realloc(
-//         ptr: *mut u8,
-//         old_size: usize,
-//         align: usize,
-//         new_size: usize,
-//     ) -> *mut u8 {
-//         // SAFETY: see the guarantees expected by `Layout::from_size_align` and
-//         // `GlobalAlloc::realloc`.
-//         unsafe {
-//             let old_layout = Layout::from_size_align_unchecked(old_size, align);
-//             System.realloc(ptr, old_layout, new_size)
-//         }
-//     }
+    #[rustc_std_internal_symbol]
+    pub unsafe extern "C" fn __rdl_realloc(
+        ptr: *mut u8,
+        old_size: usize,
+        align: usize,
+        new_size: usize,
+    ) -> *mut u8 {
+        // SAFETY: see the guarantees expected by `Layout::from_size_align` and
+        // `GlobalAlloc::realloc`.
+        unsafe {
+            let old_layout = Layout::from_size_align_unchecked(old_size, align);
+            System.realloc(ptr, old_layout, new_size)
+        }
+    }
 
-//     #[rustc_std_internal_symbol]
-//     pub unsafe extern "C" fn __rdl_alloc_zeroed(size: usize, align: usize) -> *mut u8 {
-//         // SAFETY: see the guarantees expected by `Layout::from_size_align` and
-//         // `GlobalAlloc::alloc_zeroed`.
-//         unsafe {
-//             let layout = Layout::from_size_align_unchecked(size, align);
-//             System.alloc_zeroed(layout)
-//         }
-//     }
-// }
+    #[rustc_std_internal_symbol]
+    pub unsafe extern "C" fn __rdl_alloc_zeroed(size: usize, align: usize) -> *mut u8 {
+        // SAFETY: see the guarantees expected by `Layout::from_size_align` and
+        // `GlobalAlloc::alloc_zeroed`.
+        unsafe {
+            let layout = Layout::from_size_align_unchecked(size, align);
+            System.alloc_zeroed(layout)
+        }
+    }
+}
